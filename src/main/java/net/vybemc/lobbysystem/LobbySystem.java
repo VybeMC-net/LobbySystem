143ee959bf82611660245237a50902f41ac304af
zflx1 <74198269+zflx1@users.noreply.github.com>
2022-04-30 23:01:07 +0200

    Registered Events, Added If-clauses for Navigator Items

--- src/main/java/net/vybemc/lobbysystem/LobbySystem.java
+++ src/main/java/net/vybemc/lobbysystem/LobbySystem.java
@@ -2,9 +2,12 @@ package net.vybemc.lobbysystem;
 
 import net.vybemc.lobbysystem.commands.BuildCommand;
 import net.vybemc.lobbysystem.events.BuildBreakListener;
+import net.vybemc.lobbysystem.events.InventoryClickListener;
 import net.vybemc.lobbysystem.events.JoinListener;
 import net.vybemc.lobbysystem.events.QuitListener;
+import net.vybemc.lobbysystem.inventories.NavigatorInventory;
 import net.vybemc.lobbysystem.util.BuildersManager;
+import net.vybemc.lobbysystem.util.CloudManager;
 import net.vybemc.lobbysystem.util.InventoryManager;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -14,6 +17,7 @@ public final class LobbySystem extends JavaPlugin {
     private static LobbySystem instance;
     private BuildersManager buildersManager;
     private InventoryManager inventoryManager;
+    private CloudManager cloudManager;
 
     @Override
     public void onEnable() {
@@ -35,8 +39,10 @@ public final class LobbySystem extends JavaPlugin {
         pm.registerEvents(new JoinListener(), this);
         pm.registerEvents(new QuitListener(), this);
         pm.registerEvents(new BuildBreakListener(), this);
+        pm.registerEvents(new InventoryClickListener(), this);
 
         pm.registerEvents(new BuildersManager(), this);
+        pm.registerEvents(new NavigatorInventory(), this);
     }
 
     private void registerInstances() {
@@ -44,6 +50,7 @@ public final class LobbySystem extends JavaPlugin {
         instance = this;
         buildersManager = new BuildersManager();
         inventoryManager = new InventoryManager();
+        cloudManager = new CloudManager();
 
     }
 
@@ -59,4 +66,7 @@ public final class LobbySystem extends JavaPlugin {
         return inventoryManager;
     }
 
+    public CloudManager getCloudManager() {
+        return cloudManager;
+    }
 }
--- src/main/java/net/vybemc/lobbysystem/inventories/NavigatorInventory.java
+++ src/main/java/net/vybemc/lobbysystem/inventories/NavigatorInventory.java
@@ -1,5 +1,9 @@
 package net.vybemc.lobbysystem.inventories;
 
+import net.vybemc.lobbysystem.LobbySystem;
+import net.vybemc.lobbysystem.util.CloudManager;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -7,13 +11,27 @@ import org.bukkit.inventory.ItemStack;
 
 public class NavigatorInventory implements Listener {
 
+    private final CloudManager cloudManager = LobbySystem.getPlugin().getCloudManager();
+
     @EventHandler
     public void onInventoryClick(InventoryClickEvent e){
+        Player p = (Player) e.getWhoClicked();
+
         if (e.getCurrentItem() != null
         && e.getInventory().getName() != null){
 
             ItemStack item = e.getCurrentItem();
             String inventoryName = e.getInventory().getName();
+
+            if (inventoryName == "§7Navigator §8» §7Main menu"){
+                if(item.getType() == Material.BED){
+                    cloudManager.sendPlayer(p, "bedwars");
+                }else if(item.getType() == Material.SANDSTONE) {
+                    cloudManager.sendPlayer(p, "buildffa");
+                }else if (item.getType() == Material.EYE_OF_ENDER){
+                    cloudManager.sendPlayer(p, "skywars");
+                }
+            }
 
         }
     }
