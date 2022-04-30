05c8e192225763fa80e1d837e59960a7ceb60636
zflx1 <74198269+zflx1@users.noreply.github.com>
2022-04-30 15:04:42 +0200

    Inventory Manager updated, Deleted old Files

--- src/main/java/net/vybemc/lobbysystem/LobbySystem.java
+++ src/main/java/net/vybemc/lobbysystem/LobbySystem.java
@@ -5,7 +5,6 @@ import net.vybemc.lobbysystem.events.BuildBreakListener;
 import net.vybemc.lobbysystem.events.JoinListener;
 import net.vybemc.lobbysystem.events.QuitListener;
 import net.vybemc.lobbysystem.util.BuildersManager;
-import net.vybemc.lobbysystem.util.ConfigManager;
 import net.vybemc.lobbysystem.util.InventoryManager;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -13,7 +12,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class LobbySystem extends JavaPlugin {
 
     private static LobbySystem instance;
-    private ConfigManager configManager;
     private BuildersManager buildersManager;
     private InventoryManager inventoryManager;
 
@@ -33,16 +31,17 @@ public final class LobbySystem extends JavaPlugin {
 
     private void registerListeners() {
         PluginManager pm = getServer().getPluginManager();
+
         pm.registerEvents(new JoinListener(), this);
         pm.registerEvents(new QuitListener(), this);
-        pm.registerEvents(new BuildersManager(), this);
         pm.registerEvents(new BuildBreakListener(), this);
+
+        pm.registerEvents(new BuildersManager(), this);
     }
 
     private void registerInstances() {
 
         instance = this;
-        configManager = new ConfigManager();
         buildersManager = new BuildersManager();
         inventoryManager = new InventoryManager();
 
@@ -50,10 +49,6 @@ public final class LobbySystem extends JavaPlugin {
 
     public static LobbySystem getPlugin() {
         return instance;
-    }
-
-    public ConfigManager getConfigManager() {
-        return configManager;
     }
 
     public BuildersManager getBuildersManager() {
--- src/main/java/net/vybemc/lobbysystem/util/InventoryManager.java
+++ src/main/java/net/vybemc/lobbysystem/util/InventoryManager.java
@@ -1,5 +1,7 @@
 package net.vybemc.lobbysystem.util;
 
+import org.bukkit.Bukkit;
+import org.bukkit.DyeColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
@@ -8,15 +10,39 @@ import org.bukkit.inventory.ItemStack;
 public class InventoryManager {
 
 
+    private void fillInventory(Inventory inventory, ItemStack item) {
+        for (int i = 0; i < inventory.getSize(); i++) {
+            inventory.setItem(i, item);
+        }
+    }
+
     public void setLobbyInventory(Player p) {
 
         Inventory inv = p.getInventory();
 
-        ItemStack navigatorItem = new ItemBuilder(Material.COMPASS).setDisplayName("§c§lNavigator §7§o(Right Click)");
-        ItemStack gadgetItem = new ItemBuilder(Material.BLAZE_ROD).setDisplayName("§6§lGadget §7§o(Right Click)");
+        ItemStack navigatorItem = new ItemBuilder(Material.COMPASS).setDisplayName("§c§lNavigator §8» §7§o(Right Click)");
+        ItemStack gadgetItem = new ItemBuilder(Material.BLAZE_ROD).setDisplayName("§6§lGadget §8» §7§o(Right Click)");
 
         inv.setItem(3, navigatorItem);
         inv.setItem(5, gadgetItem);
+
+    }
+
+    public void openNavigatorMainInventory(Player p) {
+        
+        Inventory inv = Bukkit.createInventory(null, 3*9, "§7Navigator §8» §7Main menu");
+
+        ItemStack buildFFAItem = new ItemBuilder(Material.SANDSTONE).setDisplayName("§e§lBuild FFA");
+        ItemStack bedwarsItem = new ItemBuilder(Material.BED).setDisplayName("§6§lBedwars");
+        ItemStack skywarsItem = new ItemBuilder(Material.EYE_OF_ENDER).setDisplayName("§b§lSkywars");
+
+        fillInventory(inv, new ItemBuilder(Material.STAINED_GLASS_PANE, DyeColor.WHITE).setDisplayName(" "));
+
+        inv.setItem(11, buildFFAItem);
+        inv.setItem(13, bedwarsItem);
+        inv.setItem(15, skywarsItem);
+
+        p.openInventory(inv);
 
     }
 
