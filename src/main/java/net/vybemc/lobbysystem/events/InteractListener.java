8ffec5c22231e03e52b0e4362af50f15325d433a
zflx1 <74198269+zflx1@users.noreply.github.com>
2022-05-03 15:54:21 +0200

    Bugfix in InteractListener

--- src/main/java/net/vybemc/lobbysystem/events/InteractListener.java
+++ src/main/java/net/vybemc/lobbysystem/events/InteractListener.java
@@ -1,13 +1,16 @@
 package net.vybemc.lobbysystem.events;
 
 import net.vybemc.lobbysystem.LobbySystem;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class InteractListener implements Listener {
 
+    @EventHandler
     public void onInteract(PlayerInteractEvent e){
         Player p = e.getPlayer();
 
@@ -15,7 +18,9 @@ public class InteractListener implements Listener {
             if (e.getAction() == Action.RIGHT_CLICK_AIR
                     || e.getAction() == Action.RIGHT_CLICK_BLOCK){
                 if (!LobbySystem.getPlugin().getBuildersManager().isBuilder(p)){
-                    LobbySystem.getPlugin().getInventoryManager().openNavigatorMainInventory(p);
+                    if (e.getMaterial() == Material.COMPASS) {
+                        LobbySystem.getPlugin().getInventoryManager().openNavigatorMainInventory(p);
+                    }
                 }
 
             }
