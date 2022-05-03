96ae4c3155812974b013d84b48a09a31c827732c
zflx1 <74198269+zflx1@users.noreply.github.com>
2022-05-03 22:01:58 +0200

    Added Sound in teleporter gadget and small issue fix

--- src/main/java/net/vybemc/lobbysystem/gadgets/TeleporterGadget.java
+++ src/main/java/net/vybemc/lobbysystem/gadgets/TeleporterGadget.java
@@ -3,6 +3,7 @@ package net.vybemc.lobbysystem.gadgets;
 import net.vybemc.lobbysystem.LobbySystem;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,29 +14,26 @@ import org.bukkit.util.Vector;
 public class TeleporterGadget implements Listener {
 
     @EventHandler
-    public void onInteract(PlayerInteractEvent e){
+    public void onInteract(PlayerInteractEvent e) {
         Player p = e.getPlayer();
 
         if (e.getAction() != null
-        && e.getMaterial() != null){
+                && e.getMaterial() != null) {
             if (e.getAction() == Action.RIGHT_CLICK_AIR
-            || e.getAction() == Action.RIGHT_CLICK_BLOCK){
-                if(e.getMaterial() == Material.BLAZE_ROD){
-                    if (!LobbySystem.getPlugin().getBuildersManager().isBuilder(p)){
-                        if(e.getAction() == Action.RIGHT_CLICK_BLOCK){
-                            if (e.getClickedBlock() != null){
-                                Location loc = e.getClickedBlock().getLocation();
-                                loc.setY(loc.getY() + 1);
-                                p.teleport(loc);
-                            }
-                        }else {
-                            Location loc = p.getLocation();
-                            Vector dir = loc.getDirection();
-                            dir.normalize();
-                            dir.multiply(7);
-                            loc.add(dir);
-                            p.teleport(loc);
-                        }
+                    || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+                if (e.getMaterial() == Material.BLAZE_ROD) {
+                    if (!LobbySystem.getPlugin().getBuildersManager().isBuilder(p)) {
+                        Location loc = p.getLocation();
+                        Vector dir = loc.getDirection();
+
+                        dir.normalize();
+                        dir.multiply(7);
+                        loc.add(dir);
+
+                        p.teleport(loc);
+
+                        p.playSound(p.getLocation(), Sound.ENDERMAN_TELEPORT, 1f, 1f);
+
                     }
                 }
             }
