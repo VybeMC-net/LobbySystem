8a59f88e04831ac2b954a91e47d41d9908271b9b
zflx1 <74198269+zflx1@users.noreply.github.com>
2022-05-08 22:47:39 +0200

    Some events cancelled

--- src/main/java/net/vybemc/lobbysystem/events/InteractListener.java
+++ src/main/java/net/vybemc/lobbysystem/events/InteractListener.java
@@ -8,7 +8,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
-public class InteractListener implements Listener {
+public class InteractListener  implements Listener {
 
     @EventHandler
     public void onInteract(PlayerInteractEvent e){
